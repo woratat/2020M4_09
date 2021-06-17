a18351ebff7259c52a68ee8055b4b856322139e4
woratat <84006033+woratat@users.noreply.github.com>
2021-06-17 15:19:21 +0700

    2 postman to table

--- script.js
+++ script.js
@@ -1,80 +1,3 @@
-// var student = {};
-// student.name = 'คุณลุง'
-// student.username = 'a@b.com'
-// student.gender = 'ชาย'
-
-
-// var student2 = {};
-// student2.name = 'คุณป้า'
-// student2.username = 'x@y.com'
-// student2.gender = 'หญิง'
-
-
-// function addStudentData(student){
-//     const output = document.getElementById('output')
-//     let row = document.createElement('div')
-//     row.classList.add("row")
-//     let columnName = document.createElement('div')
-//     columnName.classList.add('col-1')
-//     columnName.classList.add('offset-1')
-//     columnName.innerHTML = 'ชื่อ'
-//     let columnValue = document.createElement('div')
-//     columnValue = document.createElement('row')
-//     columnValue.classList.add('col')
-//     columnValue.innerHTML = student.name
-//     row.appendChild(columnName)
-//     row.appendChild(columnValue)
-//     output.appendChild(row)
-
-//     row = document.createElement('div')
-//     row.classList.add("row")
-//     columnName = document.createElement('div')
-//     columnName.classList.add('col-1')
-//     columnName.classList.add('offset-1')
-//     columnName.innerHTML = 'รหัส'
-//     columnValue = document.createElement('div')
-//     columnValue = document.createElement('row')
-//     columnValue.classList.add('col')
-//     columnValue.innerHTML = student.username
-//     row.appendChild(columnName)
-//     row.appendChild(columnValue)
-//     output.appendChild(row)
-
-//     row = document.createElement('div')
-//     row.classList.add("row")
-//     columnName = document.createElement('div')
-//     columnName.classList.add('col-1')
-//     columnName.classList.add('offset-1')
-//     columnName.innerHTML = 'เพศ'
-//     columnValue = document.createElement('div')
-//     columnValue = document.createElement('row')
-//     columnValue.classList.add('col')
-//     columnValue.innerHTML = student.gender
-//     row.appendChild(columnName)
-//     row.appendChild(columnValue)
-//     output.appendChild(row)
-
-// }
-// window.addEventListener("load", function(){  //เมื่อ html วิโด้วโหลดให้เรียก addStudentData()
-//     addStudentData(student)
-// })
-
-// var student={
-//     name: 'สมรักษ์',
-//     username: 'm@n.com',
-//     gender: 'ชาย',
-// }
-// var student2={
-//     name: 'สมชาย',
-//     username: 'm@n.com',
-//     gender: 'ชาย'
-// }
-
-// var students = [
-//     student,
-//     student2,
-// ]
-
 function addStudentToTable(index,student){
     const tableBody = document.getElementById('tableBody')
     let row = document.createElement('tr')
@@ -82,22 +5,20 @@ function addStudentToTable(index,student){
     cell.innerHTML = index
     row.appendChild(cell)
     cell = document.createElement('td')
-    cell.innerHTML = student.name
+    cell.innerHTML = `${student.name} ${student.surname}`
     row.appendChild(cell)
     cell = document.createElement('td')
-    // cell.innerHTML = student.username
     let img = document.createElement('img')
-    img.setAttribute('src',student.imageLink)
+    img.setAttribute('src',student.image)
+    img.height = 100
+    img.classList.add('img-thumbnail')
     cell.appendChild(img)
     row.appendChild(cell)
     cell = document.createElement('td')
-    cell.innerHTML = student.gender
+    cell.innerHTML = student.gpa
     row.appendChild(cell)
     tableBody.appendChild(row)
 }
-// window.addEventListener('load',function(){
-//     addStudentToTable(1,student)
-// })
 
 function addStudentList(studentList){
     let counter = 1
@@ -105,16 +26,65 @@ function addStudentList(studentList){
         addStudentToTable(counter++,student)
     }
 }
-// window.addEventListener('load',function(){
-//     addStudentList(students)
-// })
+
+function addStudentData(student){
+    let idElem = document.getElementById('id')
+    idElem.innerHTML = student.id
+    let studentIdElem = document.getElementById('studentId')
+    studentIdElem.innerHTML = student.studentId
+    let nameElem = document.getElementById('name')
+    nameElem.innerHTML = `${student.name} ${student.surname}`
+    let gpaElem = document.getElementById('gpa')
+    gpaElem.innerHTML = student.gpa
+    let profileElem = document.getElementById('image')
+    profileElem.setAttribute('src', student.image)
+}
 
 function onLoad(){
-    fetch('asset/students2.json').then(response =>{  //then = response object
+    fetch('https://dv-student-backend-2019.appspot.com/students').then(response =>{
         return response.json()
     })
     .then(data=>{
-        let x = data
         addStudentList(data)
+    })
+    // deleteStudent(22)
+}
+
+document.getElementById('searchButton').addEventListener('click',(event)=>{
+    let id = document.getElementById('inputText').value
+    fetch(`https://dv-student-backend-2019.appspot.com/student/${id}`).then(response=>{
+        return response.json()
+    }).then(student=>{
+        addStudentData(student)
+    })
+})
+
+function addStudentToDB(student){
+    fetch('https://dv-student-backend-2019.appspot.com/students',{
+        method: 'POST',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(student)
+    }).then(response =>{
+        return response.json()
+    }).then(data=>{
+        console.log('success',data)
+    })
+}
+
+function deleteStudent(id){
+    fetch(`https://dv-student-backend-2019.appspot.com/student${id}`,{
+        method: 'DELETE',
+    }).then(response =>{
+        if(response.status===200){
+            return response.json()
+        }else{
+            throw Error(response.statusText)
+        }
+    }).then(data=>{
+        alert(`student name ${data.name} is now deleted`)
+    }).catch(error=>{
+        alert('your input student id is not in the database')
     })
 }
