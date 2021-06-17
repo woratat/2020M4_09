7acef88ce3ffe3889c91f1081eb331a5fb29c59b
woratat <84006033+woratat@users.noreply.github.com>
2021-06-17 21:39:36 +0700

    7.Homework

--- homework.js
+++ homework.js
@@ -1,8 +1,12 @@
 function addToTable(index,student){
     const tableBody = document.getElementById('tableBody')
     let row = document.createElement('tr')
-    let cell = document.createElement('td')
-    cell = index
+    row.addEventListener('click',(event)=>{
+        console.log('click')
+        showOne(student.id)
+    })
+    let cell = document.createElement('th')
+    cell.innerHTML = index
     row.appendChild(cell)
     cell = document.createElement('td')
     cell.innerHTML = student.name
@@ -20,3 +24,49 @@ function addToList(data){
         addToTable(index++,student)
     }
 }
+function onLoad(){
+    showAll()
+}
+function showAll(){
+    fetch('https://dv-student-backend-2019.appspot.com/students').then(response=>{
+        return response.json()
+    }).then(data=>{
+        addToList(data)
+    })
+}
+
+var displayStudent = document.getElementById('displayStudent')
+var output = document.getElementById('output')
+
+function showDetail(id,one){
+    let id1 = document.getElementById('id')
+    id1.innerHTML = id
+    let studentId1 = document.getElementById('studentId')
+    studentId1.innerHTML = one.studentId
+    let name1 = document.getElementById('name')
+    name1.innerHTML = one.name
+    let surname1 = document.getElementById('surname')
+    surname1.innerHTML = one.surname
+    let gpa = document.getElementById('gpa')
+    gpa.innerHTML = one.gpa
+    let image = document.getElementById('image')
+    image.setAttribute('src',one.image)
+    image.setAttribute('width',"304")
+}
+function showOne(id){
+    fetch(`https://dv-student-backend-2019.appspot.com/student/${id}`).then(response=>{
+        return response.json()
+    }).then(data=>{
+        showDetail(data.id,data)
+    })
+}
+function hideAll(){
+    displayStudent.style.display='none'
+    output.style.display='none'
+}
+document.getElementById('allStudentMenu').addEventListener('click',(event)=>{
+    hideAll()
+    displayStudent.style.display='block'
+    output.style.display='block'
+    showAll()
+})
